4fb25ea44544a3aca1a2bb35521504bfd8a82936
Sindet <morrisindet@gmail.com>
2024-07-30 23:44:15 +0300

    fix(registrationform):render forms to html template

--- tracker/auth/routes.py
+++ tracker/auth/routes.py
@@ -1,4 +1,8 @@
-from flask import Flask, Blueprint, render_template
+from tracker import db
+from flask import Flask, Blueprint, render_template,flash,url_for,redirect
+from werkzeug.security import generate_password_hash, check_password_hash
+from tracker.auth.forms import RegistrationForm
+from tracker.model import User 
 
 auth = Blueprint("auth", __name__, template_folder="templates")
 
@@ -11,4 +15,16 @@ def login():
 
 @auth.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("users/register.html")
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        try:
+            db.session.add(user)
+            db.session.commit()
+            flash('Registration successful! Please log in.', 'success')
+            return redirect(url_for('auth.login'))
+        except:
+            flash('Username already exists!', 'danger')
+            return redirect(url_for('auth.register'))
+    return render_template("users/register.html", form=form)
