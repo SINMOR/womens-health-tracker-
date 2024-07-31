b9e1cf0d04acfbb44ca98400ed1de51e8022f860
Sindet <morrisindet@gmail.com>
2024-07-31 18:19:48 +0300

    fix(user registration form):render form template

--- tracker/auth/forms.py
+++ tracker/auth/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, SubmitField, TextAreaField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from email_validator import validate_email, EmailNotValidError
 
 
 class RegistrationForm(FlaskForm):
@@ -14,3 +15,12 @@ class RegistrationForm(FlaskForm):
         "Confirm Password", validators=[DataRequired(), EqualTo("password")]
     )
     submit = SubmitField("Register")
+
+class LoginForm(FlaskForm):
+    email = StringField("Email Address", validators=[DataRequired(), Email()])
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=2, max=20)]
+    )
+    password = PasswordField("Password", validators=[DataRequired()])
+   
+    submit = SubmitField("Login")
